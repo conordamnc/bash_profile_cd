f115c94879cc9cc4c992a2853538fb5b68893c46
Conor Dempsey <conor@mopinion.com>
2026-03-28 01:55:53 +0100

    fix: Add neovom config, added upto date gitignore and pre-commit

---
+++ nvim_config/lua/custom/plugins.lua
@@ -0,0 +1,25 @@
+{
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+      
+        "python",
+        "json",
+        "bash",
+        "javascript",
+        "dockerfile",
+        "sql",
+
+        -- web dev 
+        "html",
+        "css",
+        "json",
+      
+        "c",
+        "zig"
+      },
+    },
+  },
